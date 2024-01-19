8104a91d80c2a8f26ca441f51c0d8c384386e3ee
prak132 <prakharissuper@gmail.com>
2024-01-18 19:28:01 -0800

    ui

--- scouting-app-2024/src/components/teleop/scored.js
+++ scouting-app-2024/src/components/teleop/scored.js
@@ -9,9 +9,9 @@ const Scored = ({ placed, teamnum }) => {
           width: '40vh',
           height: '5vh',
           background: '#1E232E',
-          boxShadow: '0px 0px 42.29999923706055px 2px rgba(185.33, 255, 229.92, 0.10) inset',
+          boxshadow: "0px 0px 42.3px 2px rgba(185, 255, 230, 0.10) inset, 0px 0px 42.3px 5px rgba(185, 255, 230, 0.25)",
           borderRadius: 10,
-          border: '1px #B9FFE6 solid',
+          border: '1px solid #B9FFE6',
           margin: 'auto',
           display: 'flex',
           flexDirection: 'column',
@@ -19,11 +19,11 @@ const Scored = ({ placed, teamnum }) => {
         }}>
           <div style={{ width: '100%', gap: 10, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
             <div style={{ width: '6vh', height: '6vh', position: 'relative', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-              <div style={{ width: 25, height: 25, position: 'absolute' }}>
+              <div style={{ width: 20, height: 20, position: 'absolute' }}>
                 <img src={Check} alt="Check" style={{ width: '100%', height: '100%' }} />
               </div>
             </div>
-            <div style={{ color: '#B9FFE6', fontSize: 22, fontWeight: '600', wordWrap: 'break-word' }}>{teamnum} Scored {placed}</div>
+            <div style={{ color: '#B9FFE6', fontSize: 18, fontWeight: '600', wordWrap: 'break-word' }}>{teamnum} Scored {placed}</div>
           </div>
         </div>
       )}
