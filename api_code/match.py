0f2d500bbe4adcb4de5ea9eca9d1210cfb0b04a8
prak132 <prakharissuper@gmail.com>
2024-01-22 19:49:17 -0800

    better error handling

--- api_code/match.py
+++ api_code/match.py
@@ -1,24 +1,27 @@
 import json
 import requests
 
-with open('data.json', 'r') as f:
-    match_codes = json.load(f)
 api_key = 'l3mMnNWP1BVGuj9iEMoqpoZb3Oe18tpmpA79GQShKGBEW63PvIO2e4ksnDDFatbw'
 matches_data = {}
+
 def get_teams_data(matchcode):
-    url = f'https://www.thebluealliance.com/api/v3/match/{matchcode}/simple'
-    headers = {
-        'accept': 'application/json',
-        'X-TBA-Auth-Key': api_key
-    }
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        data = response.json()
-        matches_data[matchcode] = data
-    else:
-        print(f"Faileded on: {matchcode}")
-for matchcode in match_codes:
+  url = f'https://www.thebluealliance.com/api/v3/match/{matchcode}/simple'
+  headers = {'accept': 'application/json', 'X-TBA-Auth-Key': api_key}
+  response = requests.get(url, headers=headers)
+  if response.status_code == 200:
+    data = response.json()
+    matches_data[matchcode] = data
+  else:
+    print(f"Failed on: {matchcode}")
+
+def main():
+  with open('match.json', 'r') as f:
+    match_codes = json.load(f)
+  for matchcode in match_codes:
     get_teams_data(matchcode)
-with open('match.json', 'w') as outfile:
+  with open('team.json', 'w') as outfile:
     json.dump(matches_data, outfile, indent=2)
-print("done")
+  print("done")
+
+if __name__ == "__main__":
+  main()
--- api_code/team.py
+++ api_code/team.py
@@ -4,15 +4,21 @@ import requests
 api_key = 'l3mMnNWP1BVGuj9iEMoqpoZb3Oe18tpmpA79GQShKGBEW63PvIO2e4ksnDDFatbw'
 event_code = '2023cala'
 url = f'https://www.thebluealliance.com/api/v3/event/{event_code}/matches/keys'
+
 headers = {
     'accept': 'application/json',
     'X-TBA-Auth-Key': api_key
 }
-response = requests.get(url, headers=headers)
-if response.status_code == 200:
-    data = response.json()
-    with open('match.json', 'w') as outfile:
-        json.dump(data, outfile, indent=2)
-    print("Done!")
-else:
-    print("Failed :(")
+
+try:
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+    if response.status_code == 200:
+        data = response.json()
+        with open('match.json', 'w') as outfile:
+            json.dump(data, outfile, indent=2)
+        print("Done!")
+    else:
+        print(f"Failed with status code {response.status_code}")
+except requests.exceptions.RequestException as e:
+    print(f"Failed. Error: {e}")
