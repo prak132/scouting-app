5eb425d023d9abf49d36adc36aae18db960a758f
DhananjayKhulbe <dkhulbe115@gmail.com>
2024-01-26 15:31:54 -0800

    buggy af code

--- scouting-app-2024/src/App.js
+++ scouting-app-2024/src/App.js
@@ -8,45 +8,49 @@ import QualEndGameLayout from "./components/qualitative/endgame/layout.js";
 import PageButtons from "./components/navigationButtons";
 import MenuElements from "./components/menus.js";
 import StartLayout from "./layout.js";
+import TextBox from "./layout.js";
+
 
 function App() {
-  const [currentPage, setCurrentPage] = useState(0);
-  const [isQuantitativeMode, setQuantitativeMode] = useState(true);
-  const qualpages = [StartLayout, AutoLayout, QualTeleopLayout, QualEndGameLayout];
-  const quantpages = [StartLayout, AutoLayout, QuantTeleopLayout, QuantEndGameLayout];
+ const [currentPage, setCurrentPage] = useState(0);
+ const [isQuantitativeMode, setIsQuantitativeMode] = useState(true);
 
-  const handleLeftButtonClick = () => {
-    setCurrentPage((prevPage) => (prevPage > 0 ? prevPage - 1 : (isQuantitativeMode ? quantpages.length - 1 : qualpages.length - 1)));
-  };
 
-  const handleRightButtonClick = () => {
-    setCurrentPage((prevPage) => (prevPage < (isQuantitativeMode ? quantpages.length - 1 : qualpages.length - 1) ? prevPage + 1 : 0));
-  };
+ const qualpages = [StartLayout, AutoLayout, QualTeleopLayout, QualEndGameLayout];
+ const quantpages = [StartLayout, AutoLayout, QuantTeleopLayout, QuantEndGameLayout];
 
-  const handleQualitativeButtonClick = () => {
-    setQuantitativeMode(false);
-  };
 
-  const handleQuantitativeButtonClick = () => {
-    setQuantitativeMode(true);
-  };
+ const handleLeftButtonClick = () => {
+   setCurrentPage(prevPage => (prevPage > 0 ? prevPage - 1 : (isQuantitativeMode ? quantpages : qualpages).length - 1));
+ };
 
-  const choosePage = () => {
-    const PageComponent = isQuantitativeMode ? quantpages[currentPage] : qualpages[currentPage];
-    return <PageComponent key={currentPage} />;
-  };
 
-  return (
-    <div>
-      <MenuElements />
-      <StartLayout
-        onQualitativeButtonClick={handleQualitativeButtonClick}
-        onQuantitativeButtonClick={handleQuantitativeButtonClick}
-      />
-      <div>{choosePage()}</div>
-      <PageButtons onLeftButtonClick={handleLeftButtonClick} onRightButtonClick={handleRightButtonClick} />
-    </div>
-  );
+ const handleRightButtonClick = () => {
+   setCurrentPage(prevPage => (prevPage < (isQuantitativeMode ? quantpages : qualpages).length - 1 ? prevPage + 1 : 0));
+ };
+
+
+ const handleSetQuantitativeMode = (mode) => {
+   setIsQuantitativeMode(mode);
+   setCurrentPage(0); // Reset currentPage when mode changes
+ };
+
+
+ const choosePage = () => {
+   const PageComponent = isQuantitativeMode ? quantpages[currentPage] : qualpages[currentPage];
+   return <div>{PageComponent && <PageComponent key={currentPage} />}</div>;
+ };
+
+
+ return (
+   <div>
+     <MenuElements/>
+     <TextBox setQuantitativeMode={handleSetQuantitativeMode} />
+     <div>{choosePage()}</div>
+     <PageButtons onLeftButtonClick={handleLeftButtonClick} onRightButtonClick={handleRightButtonClick} />
+   </div>
+ );
 }
+
 
 export default App;
