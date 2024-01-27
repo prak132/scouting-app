cabc5fcf8d177d19dd593241d3609774bd592daa
aravindkrishna2008 <86750995+aravindkrishna2008@users.noreply.github.com>
2024-01-27 14:48:38 -0800

    added notes

--- scouting-app-2024/src/components/auto/layout.js
+++ scouting-app-2024/src/components/auto/layout.js
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
-import BlueAuto from './assets/blue_auto.svg';
-import './layout.css';
+import BlueAuto from "./assets/blue_auto.svg";
+import "./layout.css";
 
 const AutoLayout = () => {
   const [selectedPosition, setSelectedPosition] = useState(null);
   const elapsedTime = 2.23;
-  const pos = ["Left", "Middle", "Right"]; 
+  const pos = ["Left", "Middle", "Right"];
   const handlePositionClick = (position) => {
     setSelectedPosition(position);
   };
@@ -26,9 +26,10 @@ const AutoLayout = () => {
               fontWeight: "700",
               wordWrap: "break-word",
               marginBottom: "-0.25vh",
-              background: 'linear-gradient(#fff, #333), -webkit-linear-gradient(#fff, #333)',
-              WebkitBackgroundClip: 'text',
-              WebkitTextFillColor: 'transparent',
+              background:
+                "linear-gradient(#fff, #333), -webkit-linear-gradient(#fff, #333)",
+              WebkitBackgroundClip: "text",
+              WebkitTextFillColor: "transparent",
             }}
           >
             Auto
@@ -46,13 +47,51 @@ const AutoLayout = () => {
       </div>
       <div className="svg-bounding">
         <img src={BlueAuto} alt="auto-img" className="auto-map" />
-        <button className="note-button"></button>
+        <div
+          style={{
+            position: "absolute",
+            display: "flex",
+            justifyContent: "space-around",
+            marginTop: "25%",
+            top: "0",
+            left: "0",
+            width: "100%",
+            height: "100%",
+          }}
+        >
+          <button className="note-button"></button>
+          <button className="note-button"></button>
+          <button className="note-button"></button>
+          <button className="note-button"></button>
+          <button className="note-button"></button>
+        </div>
+        <div
+          style={{
+            position: "absolute",
+            display: "flex",
+            justifyContent: "space-around",
+            marginTop: "25%",
+            top: "0",
+            left: "0",
+            width: "100%",
+            height: "100%",
+            marginTop: "105%",
+            width: "50%",
+            marginLeft: "46%",
+          }}
+        >
+          <button className="note-button"></button>
+          <button className="note-button"></button>
+          <button className="note-button"></button>
+        </div>
       </div>
       <div className="position-bounds">
         {pos.map((position, index) => (
           <div
             key={index}
-            className={`position-button blue-${position.toLowerCase()} ${selectedPosition === position ? "selected" : ""}`}
+            className={`position-button blue-${position.toLowerCase()} ${
+              selectedPosition === position ? "selected" : ""
+            }`}
             onClick={() => handlePositionClick(position)}
           >
             {position}
