c74666497bd841e7b4183b583687ccffce4b0fa2
bkftlettuce <lauchenxia@gmail.com>
2024-01-28 22:15:55 -0800

    UI improvements
    better icons + quick popup fix (still bad tho)

--- scouting-app-2024/src/components/menus.js
+++ scouting-app-2024/src/components/menus.js
@@ -4,8 +4,6 @@ import useSound from 'use-sound';
 import Logo from "./assets/monkeylogo.svg";
 import "./menu.css";
 import Cookies from "js-cookie";
-import BlueetoothIcon from "./assets/blueeetooth.svg";
-import ResetIcon from "./assets/resett.svg";
 import BluetoothDeviceConnected from "./assets/ready.mp3"
 
 const MenuElements = ({ onTopButtonClick, onBottomButtonClick, onMonkeyClick }) => {
@@ -118,17 +116,18 @@ const MenuElements = ({ onTopButtonClick, onBottomButtonClick, onMonkeyClick }) 
         <img src={Logo} alt='funkylogo' className='logo' onClick={handleLogoClick}></img>
         <div className='topButtons'>
           <button onClick={refreshPage} className='button topButton'>
-            <img src={ResetIcon} alt="RESETBUTTON" />
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className='icon'><path d="M463.5 224H472c13.3 0 24-10.7 24-24V72c0-9.7-5.8-18.5-14.8-22.2s-19.3-1.7-26.2 5.2L413.4 96.6c-87.6-86.5-228.7-86.2-315.8 1c-87.5 87.5-87.5 229.3 0 316.8s229.3 87.5 316.8 0c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0c-62.5 62.5-163.8 62.5-226.3 0s-62.5-163.8 0-226.3c62.2-62.2 162.7-62.5 225.3-1L327 183c-6.9 6.9-8.9 17.2-5.2 26.2s12.5 14.8 22.2 14.8H463.5z"/></svg>
           </button>
           <button onClick={sendDataToBluetooth} className='button middleButton'>
-            <img src={BlueetoothIcon} alt="BTE" />
-          </button>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" className='icon'><path d="M579.8 267.7c56.5-56.5 56.5-148 0-204.5c-50-50-128.8-56.5-186.3-15.4l-1.6 1.1c-14.4 10.3-17.7 30.3-7.4 44.6s30.3 17.7 44.6 7.4l1.6-1.1c32.1-22.9 76-19.3 103.8 8.6c31.5 31.5 31.5 82.5 0 114L422.3 334.8c-31.5 31.5-82.5 31.5-114 0c-27.9-27.9-31.5-71.8-8.6-103.8l1.1-1.6c10.3-14.4 6.9-34.4-7.4-44.6s-34.4-6.9-44.6 7.4l-1.1 1.6C206.5 251.2 213 330 263 380c56.5 56.5 148 56.5 204.5 0L579.8 267.7zM60.2 244.3c-56.5 56.5-56.5 148 0 204.5c50 50 128.8 56.5 186.3 15.4l1.6-1.1c14.4-10.3 17.7-30.3 7.4-44.6s-30.3-17.7-44.6-7.4l-1.6 1.1c-32.1 22.9-76 19.3-103.8-8.6C74 372 74 321 105.5 289.5L217.7 177.2c31.5-31.5 82.5-31.5 114 0c27.9 27.9 31.5 71.8 8.6 103.9l-1.1 1.6c-10.3 14.4-6.9 34.4 7.4 44.6s34.4 6.9 44.6-7.4l1.1-1.6C433.5 260.8 427 182 377 132c-56.5-56.5-148-56.5-204.5 0L60.2 244.3z"/></svg>          </button>
           <Popup
-            trigger={<button className='button popupButton' style = {{fontSize:'26px'}}>?</button>}
+            trigger={<button className='button popupButton' style = {{fontSize:'12px'}}>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 192 512" className='icon'><path d="M48 80a48 48 0 1 1 96 0A48 48 0 1 1 48 80zM0 224c0-17.7 14.3-32 32-32H96c17.7 0 32 14.3 32 32V448h32c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H64V256H32c-17.7 0-32-14.3-32-32z"/></svg>
+          </button>}
             position="middle"
             modal
           >
-            <div className="popup-content">
+            <div className="popup">
               <h2>How To Use</h2>
               <p>
                 <li>1. Click on either the Red or Blue alliance.</li> 
@@ -138,7 +137,7 @@ const MenuElements = ({ onTopButtonClick, onBottomButtonClick, onMonkeyClick }) 
                 <li>5. Assess scoring defense for qualitative insights.</li>
                 <li>6. Fill out the endgame section based on the chosen assessment method.</li> 
               </p>
-              <button onClick={() => {}}>close</button>
+              <div className='bottom-element'>Click anywhere to close this window</div>
             </div>
           </Popup>
         </div>
--- scouting-app-2024/src/components/navigationButtons.js
+++ scouting-app-2024/src/components/navigationButtons.js
@@ -35,32 +35,24 @@ const PageButtons = ({ onLeftButtonClick, onRightButtonClick }) => {
             backgroundColor: '#313B54',
           }}
         ></div>
-
         <button
           onClick={onLeftButtonClick}
           style={{
-            color: '#FFF',
-            fontFamily: 'Poppins',
-            fontSize: '6vw',
             backgroundColor: 'transparent',
             border: 'none',
-            marginLeft: 20,
-          }}
-        >
-          {'<'}
-        </button>
+            width: '40px',
+            margin: 10,
+          }}>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" style={{position: 'relative', width: '18px'}}><path fill="#ffffff" d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z"/></svg>        </button>
         <button
           onClick={onRightButtonClick}
           style={{
-            color: '#FFF',
-            fontFamily: 'Poppins',
-            fontSize: '6vw',
             backgroundColor: 'transparent',
             border: 'none',
-            marginRight: 20,
-          }}
-        >
-          {'>'}
+            width: '40px',
+            margin: 10,
+          }}>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" style={{position: 'relative', width: '18px'}}><path fill="#ffffff" d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"/></svg>
         </button>
       </div>
     </div>
