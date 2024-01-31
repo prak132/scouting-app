2eaa7df6b6a7285926e378d957c0f77c0b221039
DhananjayKhulbe <dkhulbe115@gmail.com>
2024-01-30 21:34:29 -0800

    button resizing

--- scouting-app-2024/src/components/menus.js
+++ scouting-app-2024/src/components/menus.js
@@ -48,7 +48,7 @@ const MenuElements = ({ onTopButtonClick, onBottomButtonClick, onMonkeyClick }) 
             </svg>
           </button>
           <div style={{ fontSize: '12px' }} onClick={toggleModal} className='button'>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 192 512" className='icon'>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 192 512" className='icon3'>
               <path fill='#454545' d="M48 80a48 48 0 1 1 96 0A48 48 0 1 1 48 80zM0 224c0-17.7 14.3-32 32-32H96c17.7 0 32 14.3 32 32V448h32c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H64V256H32c-17.7 0-32-14.3-32-32z"/>
             </svg>
           </div>
