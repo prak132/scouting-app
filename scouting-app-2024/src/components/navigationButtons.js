5325ebeecd51ef7f4bfee96e5c5dc0a9bfdb6831
Prakhar <100105525+prak132@users.noreply.github.com>
2024-01-30 20:09:23 -0800

    hashing/devkey

--- scouting-app-2024/src/components/navigationButtons.js
+++ scouting-app-2024/src/components/navigationButtons.js
@@ -1,6 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const PageButtons = ({ onLeftButtonClick, onRightButtonClick }) => {
+  const [devModeKey, setDevModeKey] = useState('');
+  // eslint-disable-next-line
+  let devmode = false;
+  const fhash = (str, seed = 0) => {
+    let h1 = 0xdeadbeef ^ seed,
+      h2 = 0x41c6ce57 ^ seed;
+    for (let i = 0, ch; i < str.length; i++) {
+      ch = str.charCodeAt(i);
+      h1 = Math.imul(h1 ^ ch, 2654435761);
+      h2 = Math.imul(h2 ^ ch, 1597334677);
+    }
+    h1 = Math.imul(h1 ^ (h1 >>> 16), 2246822507) ^ Math.imul(h2 ^ (h2 >>> 13), 3266489909);
+    h2 = Math.imul(h2 ^ (h2 >>> 16), 2246822507) ^ Math.imul(h1 ^ (h1 >>> 13), 3266489909);
+    return 4294967296 * (2097151 & h2) + (h1 >>> 0);
+  };
+  
+  const handleInputChange = (event) => {
+    const inputValue = event.target.value;
+    setDevModeKey(inputValue);
+    if(fhash(inputValue) === 7067371117425878) {
+      console.log("dev mode enabled");
+      devmode = true;
+    }
+  };
+
   return (
     <div
       style={{
@@ -44,6 +69,19 @@ const PageButtons = ({ onLeftButtonClick, onRightButtonClick }) => {
             margin: 10,
           }}>
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" style={{position: 'relative', width: '18px'}}><path fill="#ffffff" d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z"/></svg>        </button>
+          <input
+          placeholder="DevMode Key"
+          value={devModeKey}
+          onChange={handleInputChange}
+          style={{
+            margin: 10,
+            padding: '5px',
+            border: '2px solid #313B54',
+            borderRadius: '4px',
+            color: '#fff',
+            backgroundColor: 'transparent',
+          }}
+        />
         <button
           onClick={onRightButtonClick}
           style={{
