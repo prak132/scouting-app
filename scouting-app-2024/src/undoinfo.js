60df771fdedf273bbd10ca0af564b97d615a7553
Prakhar <100105525+prak132@users.noreply.github.com>
2024-02-23 18:27:06 -0800

    undo button

--- scouting-app-2024/src/undoinfo.js
+++ scouting-app-2024/src/undoinfo.js
@@ -1,5 +1,4 @@
 import React from "react";
-import RedoMark from "./assets/redo.svg";
 import ExclamationMark from "./assets/exclamation.svg";
 
 const UndoDev = ({ onUndoClick, onInfoClick }) => {
@@ -16,9 +15,11 @@ const UndoDev = ({ onUndoClick, onInfoClick }) => {
          padding: 0, 
          margin: 0 }}>
         <div style={{ justifyContent: 'center', alignItems: 'center', gap: 12, display: 'flex' }}>
-          <div style={{ width: 36, height: 36, position: 'relative', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-            <div style={{ width: 36, height: 36, borderRadius: '50%', border: '1px rgba(255, 255, 255, 0.70) solid', overflow: 'hidden' }}>
-              <img src={RedoMark} alt="Redo" style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
+          <div style={{ width: 80, height: 36, position: 'relative', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            <div style={{ width: 80, height: 36, borderRadius: '10%', border: '1px rgba(255, 255, 255, 0.70) solid', overflow: 'hidden', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+              <div style={{color: "white", fontSize: "18px"}}>
+                UNDO
+              </div>
             </div>
           </div>
         </div>
