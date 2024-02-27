2600c76fb4a3ec1e1e526de9deafe319ef99cd71
Prakhar <100105525+prak132@users.noreply.github.com>
2024-02-26 19:22:47 -0800

    fix

--- scouting-app-2024/src/components/devPage.js
+++ scouting-app-2024/src/components/devPage.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
+import matchesData from "../data/match.json";
 
-const DevPage = ({ blueTeamNumbers, redTeamNumbers, selAlliance, devRows, devSetRows, devTeamOptions, devSetTeamOptions, devScoredTeams, devSetScoredTeams, handleFinishClick }) => {
+const DevPage = ({ blueTeamNumbers, redTeamNumbers, selAlliance, matchValue, setMatchValue, devRows, devSetRows, devTeamOptions, devSetTeamOptions, devScoredTeams, devSetScoredTeams, handleFinishClick }) => {
   useEffect(() => {
     const blueTeams = Array.isArray(blueTeamNumbers) ? blueTeamNumbers : [];
     const redTeams = Array.isArray(redTeamNumbers) ? redTeamNumbers : [];
@@ -15,6 +16,10 @@ const DevPage = ({ blueTeamNumbers, redTeamNumbers, selAlliance, devRows, devSet
     devSetTeamOptions(options);
     // eslint-disable-next-line
   }, [blueTeamNumbers, redTeamNumbers]);
+
+  const handleMatchChange = (e) => {
+    setMatchValue(e.target.value);
+  };
 
   useEffect(() => {
     console.log(devScoredTeams);
@@ -49,7 +54,7 @@ const DevPage = ({ blueTeamNumbers, redTeamNumbers, selAlliance, devRows, devSet
           display: 'flex',
           flexDirection: 'column',
           position: 'relative',
-          maxHeight: '250px',
+          maxHeight: '400px',
           overflowY: 'scroll',
           padding: '14px',
           backgroundColor: '#000614',
@@ -57,113 +62,128 @@ const DevPage = ({ blueTeamNumbers, redTeamNumbers, selAlliance, devRows, devSet
           boxShadow: '0px 0px 35px 1px rgba(255, 255, 255, 0.1)',
         }}
       >
-        {devRows.map((devRows, index) => (
-          <div
-            key={devRows}
-            className="row"
-            style={{
-              marginBottom: '13px',
-              display: 'flex',
-              justifyContent: 'space-between',
-            }}
-          >
-            <select
-              className="team-select"
-              onChange={(e) => handleTeamSelect(index, e.target.value)}
-              value={devScoredTeams[index]?.[0] ? `team${devScoredTeams[index][0]}` : ""}
-              style={{
-                width: '100px',
-                flex: 0.4,
-                flexShrink: 0,
-                padding: '8px',
-                backgroundColor: '#000614',
-                border: '1px solid #2F3953',
-                borderRadius: '10px',
-                color: 'rgba(255, 255, 255, 0.7)',
-                fontFamily: 'Poppins',
-                fontSize: '16px',
-                fontStyle: 'normal',
-                fontWeight: '600',
-                lineHeight: 'normal',
-                boxShadow: '2px 0px 22px 1px rgba(255, 255, 255, 0.1)',
-              }}
-            >
-              {devTeamOptions}
-            </select>
-            <select
-              className="defense-select"
-              onChange={(e) => handleDefenseSelect(index, e.target.value)}
-              value={devScoredTeams[index]?.[1] || ""}
-              style={{
-                marginLeft: '10px',
-                width: '100px',
-                flex: 1,
-                padding: '8px',
-                fontFamily: 'Poppins',
-                fontSize: '16px',
-                color: 'rgba(255, 255, 255, 0.7)',
-                backgroundColor: '#000614',
-                border: '1px solid #2F3953',
-                borderRadius: '10px',
-                boxShadow: '0px 0px 35px 1px rgba(255, 255, 255, 0.1)',
-              }}
-            >
-              <option value="">Defense</option>
-              {[1, 2, 3, 4, 5].map((number) => (
-                <option key={number} value={number}>
-                  {number}
-                </option>
-              ))}
-            </select>
+        <div>
+          <input
+            type="text"
+            list="matchOptions"
+            value={matchValue}
+            onChange={handleMatchChange}
+            placeholder="Select a match"
+            className="glowing-dropdown-input text"
+            id="matchInput"
+          />
+          <datalist id="matchOptions">
+            {matchesData.map((matchKey, index) => (
+              <option key={index} value={matchKey} />
+            ))}
+          </datalist>
+        </div>
+          <div style={{ display: 'flex', marginTop: '10px', marginBottom: '10px',}}>
             <button
-              className="delete-button"
-              onClick={() => removeRow(index)}
+              className="add-button"
+              onClick={addRow}
               style={{
-                marginLeft: '10px',
+                alignSelf: 'flex-start',
+                marginRight: '10px',
                 backgroundColor: 'transparent',
                 color: 'rgba(255, 255, 255, 0.7)',
-                fontSize: '30px',
-                border: 'none',
+                fontSize: '25px',
+                border: '1px solid #2F3953',
                 borderRadius: '10px',
                 boxShadow: '0px 0px 35px 1px rgba(255, 255, 255, 0.1)',
               }}
             >
-              -
+              +
             </button>
-          </div>
-        ))}
-        <button
-          className="add-button"
-          onClick={addRow}
+              <button
+                className="finish-button"
+                onClick={handleFinishClick}
+                style={{
+                  marginRight: '10px',
+                  backgroundColor: 'transparent',
+                  color: 'rgba(255, 255, 255, 0.7)',
+                  fontSize: '25px',
+                  border: '1px solid #2F3953',
+                  borderRadius: '10px',
+                  boxShadow: '0px 0px 35px 1px rgba(255, 255, 255, 0.1)',
+                }}
+              >
+                Finish
+              </button>
+            </div>
+            {devRows.map((devRows, index) => (
+        <div
+          key={devRows}
+          className="row"
           style={{
-            alignSelf: 'flex-start',
-            marginTop: '10px',
-            backgroundColor: 'transparent',
-            color: 'rgba(255, 255, 255, 0.7)',
-            fontSize: '25px',
-            border: '1px solid #2F3953',
-            borderRadius: '10px',
-            boxShadow: '0px 0px 35px 1px rgba(255, 255, 255, 0.1)',
+            marginBottom: '13px',
+            display: 'flex',
+            justifyContent: 'space-between',
           }}
         >
-          +
-        </button>
-        <button
-          className="finish-button"
-          onClick={handleFinishClick}
-          style={{
-            alignSelf: 'flex-start',
-            marginTop: '10px',
-            backgroundColor: 'transparent',
-            color: 'rgba(255, 255, 255, 0.7)',
-            fontSize: '25px',
-            border: '1px solid #2F3953',
-            borderRadius: '10px',
-            boxShadow: '0px 0px 35px 1px rgba(255, 255, 255, 0.1)',
-          }}
-        >
-          Finish
-        </button>
+          <input
+            className="team-input"
+            onChange={(e) => handleTeamSelect(index, e.target.value)}
+            value={devScoredTeams[index]?.[0] || ""}
+            style={{
+              width: '100px',
+              flex: 0.4,
+              flexShrink: 0,
+              padding: '8px',
+              backgroundColor: '#000614',
+              border: '1px solid #2F3953',
+              borderRadius: '10px',
+              color: 'rgba(255, 255, 255, 0.7)',
+              fontFamily: 'Poppins',
+              fontSize: '16px',
+              fontStyle: 'normal',
+              fontWeight: '600',
+              lineHeight: 'normal',
+              boxShadow: '2px 0px 22px 1px rgba(255, 255, 255, 0.1)',
+            }}
+          />
+          <select
+            className="defense-select"
+            onChange={(e) => handleDefenseSelect(index, e.target.value)}
+            value={devScoredTeams[index]?.[1] || ""}
+            style={{
+              marginLeft: '10px',
+              width: '100px',
+              flex: 1,
+              padding: '8px',
+              fontFamily: 'Poppins',
+              fontSize: '16px',
+              color: 'rgba(255, 255, 255, 0.7)',
+              backgroundColor: '#000614',
+              border: '1px solid #2F3953',
+              borderRadius: '10px',
+              boxShadow: '0px 0px 35px 1px rgba(255, 255, 255, 0.1)',
+            }}
+          >
+            <option value="">Defense</option>
+            {[1, 2, 3, 4, 5].map((number) => (
+              <option key={number} value={number}>
+                {number}
+              </option>
+            ))}
+          </select>
+          <button
+            className="delete-button"
+            onClick={() => removeRow(index)}
+            style={{
+              marginLeft: '10px',
+              backgroundColor: 'transparent',
+              color: 'rgba(255, 255, 255, 0.7)',
+              fontSize: '30px',
+              border: 'none',
+              borderRadius: '10px',
+              boxShadow: '0px 0px 35px 1px rgba(255, 255, 255, 0.1)',
+            }}
+          >
+            -
+          </button>
+        </div>
+      ))}
       </div>
     </div>
   );
