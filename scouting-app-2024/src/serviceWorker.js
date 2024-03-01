4dfa664160a5fa5fce8898ae309c63f94518749f
Prakhar <100105525+prak132@users.noreply.github.com>
2024-02-29 19:25:30 -0800

    f

--- scouting-app-2024/src/serviceWorker.js
+++ scouting-app-2024/src/serviceWorker.js
@@ -68,9 +68,9 @@ function updateCache(request, response) {
 /* eslint-disable no-restricted-globals */
 
 const isLocalhost = Boolean(
-    window.location.hostname === 'localhost' ||
-      window.location.hostname === '[::1]' ||
-      window.location.hostname.match(
+    self.location.hostname === 'localhost' ||
+      self.location.hostname === '[::1]' ||
+      self.location.hostname.match(
         /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
       )
   );
