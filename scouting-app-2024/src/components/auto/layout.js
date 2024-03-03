9fd710b0a6c8365af19cff6c07338c467e5ef55c
Prakhar <100105525+prak132@users.noreply.github.com>
2024-03-03 15:28:20 -0800

    quick fixes

--- scouting-app-2024/src/components/auto/layout.js
+++ scouting-app-2024/src/components/auto/layout.js
@@ -40,7 +40,7 @@ const AutoLayout = ({ time, clickedNotes, setClickedNotes, isPreNoteScored, onli
     <div>
       <Notif contents={"Note Scored"} launchNotif={showNotif} setLaunchNotif={setShowNotif} />
       
-      <div style={{ paddingTop: "20vh" }}>
+      <div style={{ paddingTop: "18vh" }}>
         <div type="teleop-text">
           <div
             className="title"
@@ -68,13 +68,13 @@ const AutoLayout = ({ time, clickedNotes, setClickedNotes, isPreNoteScored, onli
           Scoring • Timer: {time.toFixed(2)}s
         </div>
       </div>
-      <div style={{ display: 'flex', justifyContent: 'center' }}>
-        <p className={somethingnonono ? "hide" : "show"} style = {{marginBottom: 0, fontFamily: 'Poppins', fontSize: '20px', fontStyle: 'normal', lineHeight: 'normal', fontWeight: '600', color: '#7d7d7d', }}>Pre Note Scored?</p>
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        <p className={somethingnonono ? "hide" : "show"} style = {{marginBottom: 0, fontFamily: 'Poppins', fontSize: '20px', fontStyle: 'normal', lineHeight: 'normal', fontWeight: '600', color: '#FFFFFF', }}>Preloaded Scored?</p>
         <button className={((isPreNoteScored === true || isPreNoteScored === false) && isPreNoteScored !== null) ? "hide" : "show"} onClick={onlickYes} style={{background: 'none', border: 'none', marginLeft: '40px',}}>
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" style={{height: '2.2em', verticalAlign: '-2.7em'}}><path fill="#cccccc" d="M256 48a208 208 0 1 1 0 416 208 208 0 1 1 0-416zm0 464A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM369 209c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0l-111 111-47-47c-9.4-9.4-24.6-9.4-33.9 0s-9.4 24.6 0 33.9l64 64c9.4 9.4 24.6 9.4 33.9 0L369 209z"/></svg>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" style={{height: '2.2em', verticalAlign: '-2.7em'}}><path fill="green" d="M256 48a208 208 0 1 1 0 416 208 208 0 1 1 0-416zm0 464A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM369 209c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0l-111 111-47-47c-9.4-9.4-24.6-9.4-33.9 0s-9.4 24.6 0 33.9l64 64c9.4 9.4 24.6 9.4 33.9 0L369 209z"/></svg>
         </button>
         <button className={((isPreNoteScored === true || isPreNoteScored === false) && isPreNoteScored !== null) ? "hide" : "show"} onClick={onlickNo} style={{background: 'none', border: 'none', marginLeft: '10px',}}>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" style={{height: '2.2em', verticalAlign: '-2.7em'}}><path fill="#cccccc" d="M256 48a208 208 0 1 1 0 416 208 208 0 1 1 0-416zm0 464A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM175 175c-9.4 9.4-9.4 24.6 0 33.9l47 47-47 47c-9.4 9.4-9.4 24.6 0 33.9s24.6 9.4 33.9 0l47-47 47 47c9.4 9.4 24.6 9.4 33.9 0s9.4-24.6 0-33.9l-47-47 47-47c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0l-47 47-47-47c-9.4-9.4-24.6-9.4-33.9 0z"/></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" style={{height: '2.2em', verticalAlign: '-2.7em'}}><path fill="red" d="M256 48a208 208 0 1 1 0 416 208 208 0 1 1 0-416zm0 464A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM175 175c-9.4 9.4-9.4 24.6 0 33.9l47 47-47 47c-9.4 9.4-9.4 24.6 0 33.9s24.6 9.4 33.9 0l47-47 47 47c9.4 9.4 24.6 9.4 33.9 0s9.4-24.6 0-33.9l-47-47 47-47c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0l-47 47-47-47c-9.4-9.4-24.6-9.4-33.9 0z"/></svg>
         </button>
       </div>
       <div className="svg-bounding">
